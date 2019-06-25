2d2f0bd4f82f6da06b205c2ed586cd2022de87f0
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2019-06-25 16:05:11 -0400

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,5 +1,19 @@
+var dir = 'http://192.168.0.1/owncloud/index.php/s/B8QddpcHgncQQ6T'
 
 module.exports.home = (req,res,next)=>{
-	var paths = ['images/a.jpg','images/b.jpg','images/c.jpg'];
+	var walkSync = function(dir, paths) {
+	  var fs = fs || require('fs'),
+	      files = fs.readdirSync(dir);
+	  paths = paths || [];
+	  files.forEach(function(file) {
+	    if (fs.statSync(dir + '/' + file).isDirectory()) {
+	      paths = walkSync(path.join(dir, file), paths);
+	    }
+	    else {
+	      paths.push(file);
+	    }
+	  });
+	  return paths;
+	}
 	res.render('gallery', { imgs: paths, layout:false});
 };
