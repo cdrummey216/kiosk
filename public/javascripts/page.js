d6c323e2d6655bfab91ea2af4aafb39329374c2d
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2019-06-27 16:04:35 -0400

    Update page.js

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,4 +1,10 @@
 jQuery(document).ready(function(){
 	Galleria.loadTheme('/galleria/themes/fullscreen/galleria.fullscreen.min.js');
-	Galleria.run('.galleria');
+	Galleria.run('.galleria', {
+		//15 min// autoplay: 900000,
+		autoplay: 7000,
+		fullscreenTransition: 'fade',
+		transitionSpeed:3000,
+		showImagenav: false
+	});
 });
