e7fbfb68287a5d84d76bc6fe4a71890ac4e3563c
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2019-06-29 10:46:08 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,4 @@ var controller = require('../controller');
 /* GET home page. */
 router.get('/', controller.home);
 
-router.get('/login', controller.login);
-
-router.get('/oauthredirect',controller.oauthredirect);
-
 module.exports = router;
