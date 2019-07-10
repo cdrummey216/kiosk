57909d4a9c185da25fa64deb0762c73da29740c1
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2019-07-09 20:31:44 -0400

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,5 +1,12 @@
 module.exports.home = (req,res,next)=>{
 	var fs = require('fs');
-	var paths = fs.readdirSync('/var/www/imgs');
+	var names = fs.readdirSync('public/images/');
+	var paths = [];
+	
+	for (var i = 0, len = names.length; i < len; i++) {
+		var s ='images/'+names[i];
+		paths.push(s);
+	};	
+	
 	res.render('gallery', { imgs: paths, layout:false});
 };
